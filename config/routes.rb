664d58f38b00fc295a24fe9cfa85f763de6d1d3b
spacemonkeythe <mladen.meseldzija@gmail.com>
2014-11-28 00:50:28 +0100

    10th commit - bootstrap, routing, tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users  
+  
+  devise_for :users
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id',  to: 'users#show', as: 'user'
+  #resources :users, only: [:show, :index]   
   resources :links
   root to: "links#index"
  
